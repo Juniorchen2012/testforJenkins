31f1dd63ef89e165717a5fc577fa84300152d16c
Juniorchen <Juniorchen2010@163.com>
2014-03-20 13:45:53 +0800

    no message

---
+++ test4jenkins/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test4jenkins
+//
+//  Created by Juniorchen on 14-3-20.
+//  Copyright (c) 2014年 Juniorchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ test4jenkins/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test4jenkins
+//
+//  Created by Juniorchen on 14-3-20.
+//  Copyright (c) 2014年 Juniorchen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
